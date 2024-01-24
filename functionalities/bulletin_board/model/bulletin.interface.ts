7033b630b6a6945ebe9d6dfcfa0d3ddb0be31387
RiccardoMiolato <riccardomiolato@gmail.com>
2024-01-24 16:15:29 +0100

    Removed undeveloped api

--- functionalities/bulletin_board/model/bulletin.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface IMessage{
-    date: Date;
-    messageText: String;
-    author: String; //Chiave dell'utente che scrive il messaggio
-    anonymous: boolean;
-    fixed: boolean;
-    commentOf: string | null; //commento a un messaggio --> Inserire chiave del messaggio interessato
-}
--- functionalities/shifts/model/shift.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface IShift{
-    name: string;
-    description: string;
-    nUser: number;
-    users: string[];
-}
